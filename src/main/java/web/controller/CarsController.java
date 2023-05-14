7e860cbd1fe5a350102e3ac5554ff4251d386f29
seriousgraf <grafnotstupid@gmail.com>
2023-05-14 14:48:20 +0300

    Initial message

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -3,16 +3,12 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
 import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class CarsController {
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,10 +1,25 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.CarDao;
 import web.models.Car;
 
 import java.util.List;
 
-public interface CarService {
-    List<Car> getCars(int count);
+@Service
 
+public class CarService {
+    private CarDao carDao;
+    @Autowired
+
+    public void CarService(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
+
+    public List<Car> getCars(int count) {
+
+        return carDao.getCars(count);
+    }
 }
